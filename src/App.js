16b8059e7b8c781bcbec172a002108f759738735
ohhyungsuh <ohhyungsuh@gmail.com>
2022-05-18 17:03:19 +0900

    little shorter, middle save

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Location from './Location.js';
+import { locals } from './data/locals.js';
 import './App.css';
 
 function Nav(props) {
@@ -20,57 +21,83 @@ function Nav(props) {
   );
 }
 
+
+export function Select1(props) {
+  let [local, setId] = useState(locals);
+  const handleChange = (e) => {
+    setId(e.target.value);
+    
+  }
+  //console.log(local);
+  return (
+    <select aria-label="Default select example" className="select-1" onChange={handleChange}>
+      <option selected>지역</option>
+      <option value="1">강남구</option>
+      <option value="2">강동구</option>
+      <option value="3">강북구</option>
+      <option value="4">강서구</option>
+      <option value="5">관악구</option>
+      <option value="6">광진구</option>
+      <option value="7">구로구</option>
+      <option value="8">금천구</option>
+      <option value="9">노원구</option>
+      <option value="10">도봉구</option>
+      <option value="11">동대문구</option>
+      <option value="12">동작구</option>
+      <option value="13">마포구</option>
+      <option value="14">서대문구</option>
+      <option value="15">서초구</option>
+      <option value="16">성동구</option>
+      <option value="17">성북구</option>
+      <option value="18">송파구</option>
+      <option value="19">양천구</option>
+      <option value="20">영등포구</option>
+      <option value="21">용산구</option>
+      <option value="22">은평구</option>
+      <option value="23">종로구</option>
+      <option value="24">중구</option>
+      <option value="25">중랑구</option>
+    </select>
+    // <select id="select-1" onChange={handleChange}>
+    //   {locals && locals.map((local) => (
+    //     <option value={local.name} key={local.id} defaultValue={local.name}>
+    //       {local.name}
+    //     </option>
+    //   ))}
+    // </select>
+  )
+}
+
+export function Select2(props) {
+  return (
+    <select aria-label="Default select example" className="select-2">
+      <option selected>카테고리</option>
+      <option value="1">양식</option>
+      <option value="2">중식</option>
+      <option value="3">한식</option>
+      <option value="4">일식</option>
+    </select>
+  )
+}
+export function Select3(props) {
+  return (
+    <select aria-label="Default select example" className="select-3">
+      <option selected>가격대</option>
+      <option value="1">1만 이하</option>
+      <option value="2">1만 ~ 2만</option>
+      <option value="3">2만 이상 </option>
+    </select>
+  )
+}
+
 function Select(props) {
   return (
     <div className="select">
       <form className="select-form">
         <div className='nav-select-wrapper'>
-          <select aria-label="Default select example" className="select-1">
-            <option selected>지역</option>
-            <option value="1">강남구</option>
-            <option value="2">강동구</option>
-            <option value="3">강북구</option>
-            <option value="4">강서구</option>
-            <option value="5">관악구</option>
-            <option value="6">광진구</option>
-            <option value="7">구로구</option>
-            <option value="8">금천구</option>
-            <option value="9">노원구</option>
-            <option value="10">도봉구</option>
-            <option value="11">동대문구</option>
-            <option value="12">동작구</option>
-            <option value="13">마포구</option>
-            <option value="14">서대문구</option>
-            <option value="15">서초구</option>
-            <option value="16">성동구</option>
-            <option value="17">성북구</option>
-            <option value="18">송파구</option>
-            <option value="19">양천구</option>
-            <option value="20">영등포구</option>
-            <option value="21">용산구</option>
-            <option value="22">은평구</option>
-            <option value="23">종로구</option>
-            <option value="24">중구</option>
-            <option value="25">중랑구</option>
-          </select>
-
-          <select aria-label="Default select example" className="select-2">
-            <option selected>카테고리</option>
-            <option value="1">양식</option>
-            <option value="2">중식</option>
-            <option value="3">한식</option>
-            <option value="4">일식</option>
-          </select>
-
-          <select aria-label="Default select example" className="select-3">
-            <option selected>가격대</option>
-            <option value="1">1만 이하</option>
-            <option value="2">1만 ~ 2만</option>
-            <option value="3">2만 이상 </option>
-          </select>
-
-          {/* 검색이 필요한가? */}
-          <button className="nav-select-button">🏸</button>
+          <Select1 />
+          <Select2 />
+          <Select3 />
         </div>
       </form>
     </div>
@@ -81,7 +108,7 @@ function Container() {
   return (
     <div className="container">
       <div className="container-wrapper">
-        <Location />
+        <Location value="value"/>
       </div>
     </div>
   )
--- src/Location.js
+++ src/Location.js
@@ -1,86 +1,101 @@
 /*global kakao*/
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { markerdata } from "./data/markerData";
+import { locals } from "./data/locals";
 
 const Location = () => {
   useEffect(() => {
     const container = document.getElementById('location');
-
     const options = {
       // 초기값 동작구청 
       center: new kakao.maps.LatLng(37.512462244674055, 126.93936257789952),
       level: 5
     };
     const map = new kakao.maps.Map(container, options);
+    let markers = [];
 
-    /* 현재 위치 표시 */
+    addMarker();
+    
+    /* 데이터 보여주기 */
+    function addMarker() {
+      markerdata.동작구.forEach((el) => {
+        const marker = new kakao.maps.Marker({
+          position: new kakao.maps.LatLng(el.LATITUDE, el.LONGITUDE),
+        });
 
+        marker.setMap(map);
+        markers.push(marker);
+
+        let infowindow = new kakao.maps.InfoWindow({
+          content: el.PLACE, // 인포윈도우에 표시할 내용
+        });
+
+        // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록
+        kakao.maps.event.addListener(marker, 'click', function () {
+          infowindow.setContent('<div style="padding:5px;font-size:12px;">' + el.PLACE + '</div>');
+          infowindow.open(map, marker);
+          //console.log(where);
+        });
+
+        kakao.maps.event.addListener(marker, "mouseout", function () {
+          infowindow.close();
+        });
+      });
+    }
+
+
+    // function displayMarker(places){
+    //   let order = document.getElementsByClassName('select-1').
+    // }
+
+    // function addMarker(position){
+    //   const marker = new kakao.maps.Marker({
+    //     position: position
+    //   });
+
+    //   marker.setMap(map);
+    //   markers.push(marker);
+
+    //   return marker;
+    // }
+
+    // function removeMarker(){
+    //   for(let i=0; i<markers.length; i++){
+    //     markers[i].setMap(null);
+    //   }
+    //   markers = [];
+    // }
+
+    /* 현재 위치 표시*/
     // function getCurrentPos() {
     //   navigator.geolocation.getCurrentPosition(function (pos) {
     //     let curLat = pos.coords.latitude, curLon = pos.coords.longitude;
-    //     let curPos = new kakao.maps.LatLng(curLat, curLon);
-
-    //     displayMarker(curPos);
+    //     const curPos = {
+    //       center: new kakao.maps.LatLng(curLat, curLon),
+    //       level: 5
+    //     };
+    //     const curMap = new kakao.maps.Map(container, curPos);
+    //     map.center(curMap);
     //   });
-
-    //   function displayMarker(curPos){
-    //     // const curMarker = new kakao.maps.Marker({
-    //     //   map: map,
-    //     //   position: curPos
-    //     // });
-    //     map.setCenter(curPos);
-    //   }
     // }
 
-    /* 데이터 보여주기 */
-    // lat->latitude, lng->longitude, title->place
-    markerdata.forEach((el) => {
-      const marker = new kakao.maps.Marker({
-        map: map,
-        position: new kakao.maps.LatLng(el.LATITUDE, el.LONGITUDE),
-      });
-
-      let infowindow = new kakao.maps.InfoWindow({
-        content: el.PLACE, // 인포윈도우에 표시할 내용
-      });
-
-      // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록
-      kakao.maps.event.addListener(
-        marker,
-        "mouseover",
-        makeOverListener(map, marker, infowindow)
-      );
-      kakao.maps.event.addListener(
-        marker,
-        "mouseout",
-        makeOutListener(infowindow)
-      );
-    });
-
-    function makeOverListener(map, marker, infowindow) {
-      return function () {
-        infowindow.open(map, marker);
-      };
-    }
-
-    function makeOutListener(infowindow) {
-      return function () {
-        infowindow.close();
-      };
-    }
   }, [])
 
-
-
-  return (
-    <div>
-      <div id="location" style={{ width: "100%", height: "80vh" }}></div>
-      <div className='MapControlView'>
+  function CurrentPos(props) {
+    return (
+      <div className='MapControlView' onClick={() => props.getCurrentPos()}>
         <div className='accessLocation'>
           <span className='screen_out'>현위치</span>
           <span className='coach_accessLocation'></span>
         </div>
-      </div>
+      </div >
+    )
+  }
+
+  return (
+    <div>
+      <div id="location" style={{ width: "100%", height: "80vh" }}></div>
+      <CurrentPos />
     </div>
   )
 }
@@ -91,3 +106,5 @@ export default Location;
   주소-좌표 변환 객체: 주소가 들어가면 좌표가 나오는건데 안쓸거같지만 유용해보여서
   const geocoder = new kakao.maps.services.Geocoder();
 */
+
+
